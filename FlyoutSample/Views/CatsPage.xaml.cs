0c06f2278c56c8c5c5d4fb6d9015c7ade6bb5ebb
Juyoung <insert@kakao.com>
2020-01-17 00:37:23 +0900

    프로젝트 생성

---
+++ FlyoutSample/Views/CatsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FlyoutSample.Views
+{
+    public partial class CatsPage : ContentPage
+    {
+        public CatsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FlyoutSample/Views/DogsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FlyoutSample.Views
+{
+    public partial class DogsPage : ContentPage
+    {
+        public DogsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
